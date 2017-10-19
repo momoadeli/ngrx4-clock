b46d287356786545413fcec39148a31b7f8a659d
Mo Moadeli <mo@deemok.com>
2017-10-19 15:21:30 -0400

    added and called timer service

---
+++ src/app/clock/clock.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ClockService {
+  private _timer: Observable<number>;
+
+  constructor() { 
+    this.startTimer();
+  }
+
+  startTimer(): void {
+    this._timer = TimerObservable.create(1000, 1000);
+    // this._subscription = timer.subscribe( (t) => {
+    //   this._tick = t;
+    // });
+  }
+
+  getTimer(): Observable<number> {
+    return this._timer;
+  }
+}
--- src/app/clock/container/clock.component.ts
+++ src/app/clock/container/clock.component.ts
@@ -1,23 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ClockService } from '../clock.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.css']
 })
-export class ClockComponent implements OnInit {
+export class ClockComponent implements OnInit, OnDestroy {
 
   private clock_color = 'Red';
+  private _subscription: Subscription;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private clockService: ClockService
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.clock_color = params['clock-color'];
 
       // In a real app: dispatch action to load the details here.
-   });
+    });
+
+    this._subscription = this.clockService.getTimer().subscribe( 
+      (timer) => {
+      console.log('timer invoked: ' + timer)
+      }, (error) => {
+      console.log('timer error')
+      }, () => {
+        console.log('timer is never done');
+      });
   }
 
+  // always unsubscribe on component exit
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
+  }
 }
